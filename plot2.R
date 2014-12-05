6ef0982831a37c006b8798b6eb6bdc04947a7734
Brian <b.taylor2@me.com>
2014-12-05 12:41:37 -0800

    add .png and .R files for plots 1 to 4
    add files for:
    plot1.png and plot1.R
    plot2.png and plot2.R
    plot3.png and plot3.R
    plot4.png and plot4.R

---
+++ plot2.R
@@ -0,0 +1,38 @@
+## plot 2
+
+## first download the dataset - use same code for plot1 to plot4
+
+filename = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(filename, destfile="dataset", method="curl")
+
+## read in data from zip file
+zipFileInfo <- unzip("dataset", list=TRUE)
+fulldataset<- read.table(unz("dataset", as.character(zipFileInfo$Name)),sep = ";", header = TRUE)
+
+## subset for selected dates 2007-02-01 and 2007-02-02
+datasubset <- fulldataset[fulldataset$Date=="1/2/2007"|fulldataset$Date=="2/2/2007",]
+
+## reformat date and time
+dt <- paste(datasubset$Date,datasubset$Time)
+timeconvert <- strptime(paste(datasubset$Date,datasubset$Time), "%d/%m/%Y %H:%M:%S")
+
+## make new set of data converted to values
+
+data <- cbind(timeconvert,datasubset[,3:9])
+for(i in 2:8){
+  data[,i]<-as.numeric(as.character(data[,i]))
+}
+
+
+## open a PNG file
+png(filename = "plot2.png", width = 480, height = 480)
+
+## now produce the plot
+with(data, plot(time,Global_active_power,
+     xlab = "", ylab="Global Active Power (kilowatts)",
+     type = "l"))
+
+
+## close the png file
+dev.off()
+
---
+++ plot4.R
@@ -0,0 +1,66 @@
+## plot 4
+
+## first download the dataset - use same code for plot1 to plot4
+
+filename = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(filename, destfile="dataset", method="curl")
+
+## read in data from zip file
+zipFileInfo <- unzip("dataset", list=TRUE)
+fulldataset<- read.table(unz("dataset", as.character(zipFileInfo$Name)),sep = ";", header = TRUE)
+
+## subset for selected dates 2007-02-01 and 2007-02-02
+datasubset <- fulldataset[fulldataset$Date=="1/2/2007"|fulldataset$Date=="2/2/2007",]
+
+## reformat date and time
+dt <- paste(datasubset$Date,datasubset$Time)
+timeconvert <- strptime(paste(datasubset$Date,datasubset$Time), "%d/%m/%Y %H:%M:%S")
+
+## make new set of data converted to values
+
+data <- cbind(timeconvert,datasubset[,3:9])
+for(i in 2:8){
+  data[,i]<-as.numeric(as.character(data[,i]))
+}
+
+
+## open a PNG file
+png(filename = "plot4.png", width = 480, height = 480)
+
+## now produce the plot
+
+### setup for 4 plots
+
+par(mfrow = c(2,2))
+
+  
+  ## top left plot
+with(data, plot(time,Global_active_power, type = "l", main = "",
+       ylab = "Global Active Power", xlab=""))
+  
+  ## top right plot
+  
+with(data, plot(time,Voltage, type = "l", main = "",
+                ylab = "Voltage", xlab="datetime"))
+
+  
+  ## bottom left plot
+with(data, plot(time,Sub_metering_1,
+                xlab = "", ylab="Energy sub metering",
+                type = "l"))
+
+with(data, lines(time,Sub_metering_2,col="red"))
+with(data, lines(time,Sub_metering_3,col="blue"))
+
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lwd=c(1,1,1),col=c("black","red","blue"),bty="n")
+  
+  ##bottom right plot
+  
+with(data, plot(time,Global_reactive_power, type = "l", main = "",
+                ylab = "Global_reactive_power", xlab="datetime"))
+
+
+## close the png file
+dev.off()
+
